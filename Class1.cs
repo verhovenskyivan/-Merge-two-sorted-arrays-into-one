dd9ecb2bdb325461c1164915fbba09e405ea2e28
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-01-04 22:16:04 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] MergeArrays(int[] arr1, int[] arr2)
+    {
+        return arr1.Concat(arr2).Distinct().OrderBy(x=>x).ToArray();
+    }
+}
